eb7656505a3d92294a041d48c682847e4aa2752a
hanshs <hansh.starkopf@gmail.com>
2023-01-28 20:15:53 +0200

    reorganise packages

--- apps/manager/src/pages/project/[id]/parameters.tsx
+++
@@ -1,5 +0,0 @@
-import ProjectLayout from "../../../components/scenes/project-layout";
-
-export default function ParametersPage() {
-  return <ProjectLayout>jou</ProjectLayout>;
-}
--- apps/runner/lib/visibility.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from "@playwright/test";
-import { waitFor } from "./wait-for";
-
-export async function verifyVisibility(page: Page, selector: string, negate = false) {
-    await waitFor(async () => Boolean(await page.$(selector)) === !negate);
-}
