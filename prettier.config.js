4ab7c92f52280a4aa5064e99e37b13d4dd76fa03
hanshs <hansh.starkopf@gmail.com>
2023-01-31 23:21:25 +0200

    update editor config

--- prettier.config.js
+++ prettier.config.js
@@ -3,10 +3,8 @@ module.exports = {
   arrowParens: 'always',
   printWidth: 120,
   singleQuote: true,
-  jsxBracketSameLine: true,
   semi: true,
   trailingComma: 'all',
   tabWidth: 2,
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
-  // tailwindConfig: "./packages/config/tailwind",
 };
