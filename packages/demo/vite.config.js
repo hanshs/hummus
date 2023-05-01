740e073820a1d9f70b8c83feb30b4fd0edb9d443
hanshs <hansh.starkopf@gmail.com>
2023-05-01 23:25:13 +0300

    remove unnecessary comments

--- packages/demo/vite.config.js
+++ packages/demo/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: 'todo-app',
   plugins: [reactRefresh()],
 });
